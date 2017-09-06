c76a29023366ebb88c967ddf4e8cf56f5e5ac6b8
Vincent Mizoules <@>
2017-09-06 16:25:01 +0200

    Add PDF version

---
+++ dockerpulls.sh
@@ -0,0 +1,8 @@
+docker pull hello-world
+docker pull ubuntu:17.10
+docker pull golang:1.8.1
+docker pull python:2.7.13-alpine
+docker pull nginx:1.13.0-alpine
+docker pull eboraas/apache
+docker pull manell/wireshark
+
